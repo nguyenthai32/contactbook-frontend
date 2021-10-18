6b8a42dd260285fc74e6ec55de31416cbcfa69c5
Thai Nguyen <thai27082001@gmail.com>
2021-10-18 22:06:10 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import router from "./router";
+import { handle } from "./common/promise";
+createApp(App)
+    .mixin({
+        methods: {
+            handle,
+        }
+    })
+    .use(router)
+    .mount('#app')
 
-createApp(App).mount('#app')
